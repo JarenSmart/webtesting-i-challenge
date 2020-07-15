3297f05c02950748245e65efc967903e84c0ad0a
jaren <jarensmart@yahoo.com>
2020-07-14 22:16:01 -0400

    MVP Complete

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -28,5 +28,60 @@ describe("enhancer unit tests", () => {
 
       expect(enhancedItem.enhancement).toBe(mockItem + 1);
     });
+    it("Keeps enhancement lvl at 20", () => {
+      let mockItem = 20;
+      let item = {
+        name: "Keyboard",
+        durability: 20,
+        enhancement: mockItem,
+      };
+
+      const enhancedItem = succeed(item);
+
+      expect(enhancedItem.enhancement).toBe(20);
+    });
+  });
+
+  describe("fails", () => {
+    it("Enhancement level < 15", () => {
+      let mockDurability = 10;
+      let mockEnhance = 12;
+      let item = {
+        name: "Noob sword",
+        durability: mockDurability,
+        enhancement: mockEnhance,
+      };
+
+      const enhancementFailure = fail(item);
+
+      expect(enhancementFailure.durability).toBe(mockDurability - 5);
+    });
+    it("Enhancement level >= 15", () => {
+      let mockDurability = 10;
+      let mockEnhance = 15;
+      let item = {
+        name: "Noob sword",
+        durability: mockDurability,
+        enhancement: mockEnhance,
+      };
+
+      const enhancementFailure = fail(item);
+
+      expect(enhancementFailure.durability).toBe(mockDurability - 10);
+    });
+    it("Enhancement level > 16", () => {
+      let mockDurability = 17;
+      let mockEnhance = 17;
+      let item = {
+        name: "Noob sword",
+        durability: mockDurability,
+        enhancement: mockEnhance,
+      };
+
+      const enhancementFailure = fail(item);
+
+      expect(enhancementFailure.durability).toBe(mockDurability - 10);
+      expect(enhancementFailure.enhancement).toBe(mockEnhance - 1);
+    });
   });
 });
